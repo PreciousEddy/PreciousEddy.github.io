5fed4ce946f64804dbc1ae3a9cfceec59b3c8dd5
PreciousEddy <edmundprecious23@gmail.com>
2024-08-30 16:33:18 +0000

    Update

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-$(document).ready(function(){
-
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
+$(document).ready(function() {
+  // Toggle menu on click
+  $('#menu').click(function() {
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
   });
 
-  $(window).on('scroll load',function(){
+  // Show/Hide the "back to top" button on scroll
+  $(window).on('scroll load', function() {
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
 
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
-
+      if ($(window).scrollTop() > 0) {
+          $('.top').fadeIn(); // Show the "back to top" button
+      } else {
+          $('.top').fadeOut(); // Hide the "back to top" button
+      }
   });
 
-  // smooth scrolling 
-  $('a[href*="#"]').on('click',function(e){
+  // Smooth scrolling to section on clicking a link
+  $('a[href*="#"]').on('click', function(e) {
+      e.preventDefault();
 
-    e.preventDefault();
-
-    $('html, body').animate({
-      scrollTop : $($(this).attr('href')).offset().top,
-    }, 500, 'linear');
-
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top,
+      }, 500, 'linear'); // Scroll animation duration: 500ms
   });
 });
